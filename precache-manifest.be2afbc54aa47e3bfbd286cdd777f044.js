ad70e0b8cc93fa78723e3c05443706fdf37e2001
Emmy70 <Emmanuelusman701@gmail.com>
2020-07-17 17:21:53 +0100

    Updates

---
+++ precache-manifest.be2afbc54aa47e3bfbd286cdd777f044.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f9fbb79e015c740b23073b1b9d3fb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ab0ff8d43b43832c360",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "6ab0ff8d43b43832c360",
+    "url": "/static/js/2.3c99b07c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c99b07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9dbd6981a33dfbbbe9",
+    "url": "/static/js/main.66652e65.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
